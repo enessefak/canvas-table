0c8e95784bc0ddd731f56be95eb370044c758830
Enes Sefa <enes.sefa.k@gmail.com>
2020-05-28 12:16:20 +0300

    Update canvas-table.js

--- canvas-table.js
+++ canvas-table.js
@@ -1,315 +1,319 @@
 "use strict";
 
 (function ($) {
-  $.fn.canvasTable = function (_options) {
-    const options = $.extend(
-      {
-        width: 500,
-        height: 500,
-        templateAreas: [],
-        images: [],
-        gap: 1,
-        fillStyle: "#fff",
-        strokeStyle: "red",
-        strokeDash: 0,
-        strokeWidth: 3,
-        visibleAlignLine: true,
-        onSelected: () => null
-      },
-      _options
-    );
+	$.fn.canvasTable = function (_options) {
+		const options = $.extend(
+			{
+				width: 500,
+				height: 500,
+				templateAreas: [],
+				images: [],
+				gap: 1,
+				fillStyle: "#fff",
+				strokeStyle: "red",
+				strokeDash: 0,
+				strokeWidth: 3,
+				visibleAlignLine: true,
+				onSelected: () => null
+			},
+			_options
+		);
 
-    const areas = [];
+		const areas = [];
 
-    const $canvas = this;
-    const ctx = $canvas[0].getContext("2d");
+		const $canvas = this;
+		const ctx = $canvas[0].getContext("2d");
 
-    const MODE = {
-      TRANSLATE: "translate",
-      CHANGE: "change",
-      SELECTION: "selection"
-    };
+		const MODE = {
+			TRANSLATE: "translate",
+			CHANGE: "change",
+			SELECTION: "selection"
+		};
 
-    let startX;
-    let startY;
-    let activeMode;
-    let editingIndex = -1;
-    let lastClickDate;
-    
-    const canvasWidth = parseInt(options.width, 10);
-    const canvasHeight = parseInt(options.height, 10);
+		let startX;
+		let startY;
+		let activeMode;
+		let editingIndex = -1;
+		let lastClickDate;
 
-    ctx.canvas.width = canvasWidth;
-    ctx.canvas.height = canvasHeight;
+		const canvasWidth = parseInt(options.width, 10);
+		const canvasHeight = parseInt(options.height, 10);
 
-    const clear = () => {
-      ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-      ctx.fillStyle = options.fillStyle;
-      ctx.fillRect(0, 0, canvasWidth, canvasHeight);
-    };
+		ctx.canvas.width = canvasWidth;
+		ctx.canvas.height = canvasHeight;
 
-    const initArea = async (_image) => {
-      const matchedRows = [];
-      const matchedColumns = [];
-      let area = {
-        name: _image.area
-      };
+		const clear = () => {
+			ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+			ctx.fillStyle = options.fillStyle;
+			ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+		};
 
-      const rowsLength = options.templateAreas.length;
-      const heightUnit = canvasHeight / rowsLength;
+		const initArea = async (_image) => {
+			const matchedRows = [];
+			const matchedColumns = [];
+			let area = {
+				name: _image.area
+			};
 
-      const columnsLength = options.templateAreas[0].split(" ").length;
-      const widthUnit = canvasWidth / columnsLength;
+			const rowsLength = options.templateAreas.length;
+			const heightUnit = canvasHeight / rowsLength;
 
-      for (let rowIndex = 0; rowIndex < rowsLength; rowIndex++) {
-        const columns = options.templateAreas[rowIndex].split(" ");
-        const matchedArea = columns.find((column) => column === _image.area);
-        if (!matchedArea) continue;
+			const columnsLength = options.templateAreas[0].split(" ").length;
+			const widthUnit = canvasWidth / columnsLength;
 
-        if (!matchedRows.includes(rowIndex)) matchedRows.push(rowIndex);
+			for (let rowIndex = 0; rowIndex < rowsLength; rowIndex++) {
+				const columns = options.templateAreas[rowIndex].split(" ");
+				const matchedArea = columns.find((column) => column === _image.area);
+				if (!matchedArea) continue;
 
-        for (let columnIndex = 0; columnIndex < columnsLength; columnIndex++) {
-          if (
-            columns[columnIndex] === _image.area &&
-            !matchedColumns.includes(columnIndex)
-          ) {
-            matchedColumns.push(columnIndex);
-          }
-        }
-      }
+				if (!matchedRows.includes(rowIndex)) matchedRows.push(rowIndex);
 
-      area.xPos = matchedColumns[0] * widthUnit + options.gap / 2;
-      area.yPos = matchedRows[0] * heightUnit + options.gap / 2;
+				for (let columnIndex = 0; columnIndex < columnsLength; columnIndex++) {
+					if (
+						columns[columnIndex] === _image.area &&
+						!matchedColumns.includes(columnIndex)
+					) {
+						matchedColumns.push(columnIndex);
+					}
+				}
+			}
 
-      area.width = matchedColumns.length * widthUnit - options.gap;
-      area.height = matchedRows.length * heightUnit - options.gap;
+			area.xPos = matchedColumns[0] * widthUnit + options.gap / 2;
+			area.yPos = matchedRows[0] * heightUnit + options.gap / 2;
 
-      const image = new Image();
-      image.crossOrigin = "anonymous";
-      image.src = _image.source;
+			area.width = matchedColumns.length * widthUnit - options.gap;
+			area.height = matchedRows.length * heightUnit - options.gap;
 
-      return new Promise((resolve, reject) => {
-        image.onerror = reject;
+			const image = new Image();
+			image.crossOrigin = "anonymous";
+			image.src = _image.source;
 
-        image.onload = () => {
-          image.width = area.width > image.width ? area.width : image.width;
-          image.height = area.height > image.height ? area.height : image.height;
+			return new Promise((resolve, reject) => {
+				image.onerror = reject;
 
-          const xDistance = image.width - area.width;
-          const yDistance = image.height - area.height;
-          image.xTranslate = -(xDistance / 2);
-          image.yTranslate = -(yDistance / 2);
+				image.onload = () => {
+					image.width = area.width > image.width ? area.width : image.width;
+					image.height = area.height > image.height ? area.height : image.height;
 
-          image.scale = _image.scale || 1;
-          image.rotate = _image.rotate || 0;
+					const xDistance = image.width - area.width;
+					const yDistance = image.height - area.height;
+					image.xTranslate = 0;
+					image.yTranslate = 0;
 
-          image.xPos = area.xPos;
-          image.yPos = area.yPos;
+					image.scale = _image.scale || 1;
+					image.rotate = _image.rotate || 0;
 
-          resolve({
-            image,
-            ...area
-          });
-        };
-      });
-    };
+					image.xPos = area.xPos - xDistance / 2;
+					image.yPos = area.yPos - yDistance / 2;
 
-    const drawArea = () => {
-      clear();
+					resolve({
+						image,
+						...area
+					});
+				};
+			});
+		};
 
-      for (let i = 0; i < areas.length; i++) {
-        const area = areas[i];
+		const drawArea = () => {
+			clear();
 
-        const cx = area.width / 2 + area.xPos;
-        const cy = area.height / 2 + area.yPos;
-        ctx.save();
-        ctx.beginPath();
-        ctx.rect(area.xPos, area.yPos, area.width, area.height);
-        ctx.translate(cx, cy);
-        ctx.scale(area.image.scale, area.image.scale);
-        ctx.rotate((Math.PI / 180) * area.image.rotate);
-        ctx.translate(-cx, -cy);
-        ctx.clip();
-        ctx.drawImage(
-          area.image,
-          area.image.xPos + area.image.xTranslate,
-          area.image.yPos + area.image.yTranslate,
-          area.image.width,
-          area.image.height
-        );
-        ctx.restore();
+			for (let i = 0; i < areas.length; i++) {
+				const area = areas[i];
 
-        if (i === editingIndex) {
-          ctx.strokeStyle = options.strokeStyle;
-          ctx.lineWidth = options.strokeWidth;
-          ctx.setLineDash([options.strokeDash, options.strokeDash]);
-          if (options.visibleAlignLine) {
-            ctx.moveTo(area.xPos + area.width, area.yPos + area.height / 2);
-            ctx.lineTo(area.xPos, area.yPos + area.height / 2);
+				const cx = area.width / 2 + area.xPos;
+				const cy = area.height / 2 + area.yPos;
+				ctx.save();
+				ctx.beginPath();
+				ctx.rect(area.xPos, area.yPos, area.width, area.height);
+				ctx.translate(cx, cy);
+				ctx.scale(area.image.scale, area.image.scale);
+				ctx.rotate((Math.PI / 180) * area.image.rotate);
+				ctx.translate(-cx, -cy);
+				ctx.clip();
+				ctx.drawImage(
+					area.image,
+					area.image.xPos + area.image.xTranslate / area.image.scale,
+					area.image.yPos + area.image.yTranslate / area.image.scale,
+					area.image.width,
+					area.image.height
+				);
+				ctx.restore();
 
-            ctx.moveTo(area.xPos + area.width / 2, area.yPos);
-            ctx.lineTo(area.xPos + area.width / 2, area.height + area.yPos);
-          }
-          ctx.stroke();
-        }
-      }
-    };
+				if (i === editingIndex) {
+					ctx.strokeStyle = options.strokeStyle;
+					ctx.lineWidth = options.strokeWidth;
+					ctx.setLineDash([options.strokeDash, options.strokeDash]);
+					if (options.visibleAlignLine) {
+						ctx.moveTo(area.xPos + area.width, area.yPos + area.height / 2);
+						ctx.lineTo(area.xPos, area.yPos + area.height / 2);
 
-    const getAreaIndexByPosition = (e) =>
-      areas.findIndex(
-        (area) =>
-          e.offsetX >= area.xPos &&
-          e.offsetX <= area.xPos + area.width &&
-          e.offsetY >= area.yPos &&
-          e.offsetY <= area.yPos + area.height
-      );
+						ctx.moveTo(area.xPos + area.width / 2, area.yPos);
+						ctx.lineTo(area.xPos + area.width / 2, area.height + area.yPos);
+					}
+					ctx.stroke();
+				}
+			}
+		};
 
-    const changeImage = (fromAreaId, toAreaId) => {
-      activeMode = MODE.SELECTION;
-      editingIndex = toAreaId;
+		const getAreaIndexByPosition = (e) =>
+			areas.findIndex(
+				(area) =>
+					e.offsetX >= area.xPos &&
+					e.offsetX <= area.xPos + area.width &&
+					e.offsetY >= area.yPos &&
+					e.offsetY <= area.yPos + area.height
+			);
 
-      const fromArea = { ...areas[fromAreaId] };
-      const toArea = { ...areas[toAreaId] };
+		const changeImage = (fromAreaId, toAreaId) => {
+			activeMode = MODE.SELECTION;
+			editingIndex = toAreaId;
 
-      fromArea.image.xTranslate = -(fromArea.image.width - toArea.width) / 2;
-      fromArea.image.yTranslate = -(fromArea.image.height - toArea.height) / 2;
+			const fromArea = { ...areas[fromAreaId] };
+			const toArea = { ...areas[toAreaId] };
 
-      toArea.image.xTranslate = -(toArea.image.width - fromArea.width) / 2;
-      toArea.image.yTranslate = -(toArea.image.height - fromArea.height) / 2;
+			fromArea.image.xTranslate = 0;
+			fromArea.image.yTranslate = 0;
 
-      fromArea.image.xPos = toArea.xPos;
-      fromArea.image.yPos = toArea.yPos;
+			toArea.image.xTranslate = 0;
+			toArea.image.yTranslate = 0;
 
-      toArea.image.xPos = fromArea.xPos;
-      toArea.image.yPos = fromArea.yPos;
+			fromArea.image.xPos =
+				toArea.xPos - (fromArea.image.width - toArea.width) / 2;
+			fromArea.image.yPos =
+				toArea.yPos - (fromArea.image.height - toArea.height) / 2;
 
-      areas[fromAreaId] = {
-        ...fromArea,
-        image: toArea.image
-      };
+			toArea.image.xPos =
+				fromArea.xPos - (toArea.image.width - fromArea.width) / 2;
+			toArea.image.yPos =
+				fromArea.yPos - (toArea.image.height - fromArea.height) / 2;
 
-      areas[toAreaId] = {
-        ...toArea,
-        image: fromArea.image
-      };
+			areas[fromAreaId] = {
+				...fromArea,
+				image: toArea.image
+			};
 
-      drawArea();
-    };
+			areas[toAreaId] = {
+				...toArea,
+				image: fromArea.image
+			};
 
-    const moveImage = (e) => {
-      const editingArea = areas[editingIndex];
-      const overOtherImageIndex = getAreaIndexByPosition(e);
+			drawArea();
+		};
 
-      if (overOtherImageIndex > -1 && overOtherImageIndex !== editingIndex) {
-        startX = -e.offsetX;
-        startY = -e.offsetY;
-        changeImage(editingIndex, overOtherImageIndex);
-      } else {
-        $canvas.css("cursor", "move");
-        editingArea.image.xTranslate = startX + e.offsetX;
-        editingArea.image.yTranslate = startY + e.offsetY;
+		const moveImage = (e) => {
+			const editingArea = areas[editingIndex];
+			const overOtherImageIndex = getAreaIndexByPosition(e);
 
-        drawArea();
-      }
-    };
+			if (overOtherImageIndex > -1 && overOtherImageIndex !== editingIndex) {
+				startX = -e.offsetX;
+				startY = -e.offsetY;
+				changeImage(editingIndex, overOtherImageIndex);
+			} else {
+				$canvas.css("cursor", "move");
+				editingArea.image.xTranslate = startX + e.offsetX;
+				editingArea.image.yTranslate = startY + e.offsetY;
 
-    const selectMode = () => {
-      activeMode = MODE.SELECTION;
-      $canvas.css("cursor", "auto");
-      drawArea();
-      options.onSelected?.(areas[editingIndex]);
-    };
+				drawArea();
+			}
+		};
 
-    const handleMouseDown = (e) => {
-      e.preventDefault();
-      const clickedAreaIndex = getAreaIndexByPosition(e);
+		const selectMode = () => {
+			activeMode = MODE.SELECTION;
+			$canvas.css("cursor", "auto");
+			drawArea();
+			options.onSelected?.(areas[editingIndex]);
+		};
 
-      if (activeMode === MODE.TRANSLATE || activeMode === MODE.CHANGE) {
-        selectMode();
-      } else if (clickedAreaIndex > -1) {
-        options.onSelected?.(areas[clickedAreaIndex]);
-        editingIndex = clickedAreaIndex;
-        const editingArea = areas[editingIndex];
-        startX = editingArea.image.xTranslate - e.offsetX;
-        startY = editingArea.image.yTranslate - e.offsetY;
-        lastClickDate = new Date();
-        activeMode = MODE.TRANSLATE;
-      }
-    };
+		const handleMouseDown = (e) => {
+			e.preventDefault();
+			const clickedAreaIndex = getAreaIndexByPosition(e);
 
-    const handleMouseUp = (e) => {
-      e.preventDefault();
-      selectMode();
-    };
+			if (activeMode === MODE.TRANSLATE || activeMode === MODE.CHANGE) {
+				selectMode();
+			} else if (clickedAreaIndex > -1) {
+				options.onSelected?.(areas[clickedAreaIndex]);
+				editingIndex = clickedAreaIndex;
+				const editingArea = areas[editingIndex];
+				startX = editingArea.image.xTranslate - e.offsetX;
+				startY = editingArea.image.yTranslate - e.offsetY;
+				lastClickDate = new Date();
+				activeMode = MODE.TRANSLATE;
+			}
+		};
 
-    const handleMouseMove = (e) => {
-      if (editingIndex < 0 || !activeMode) return;
-      e.preventDefault();
-      if (activeMode === MODE.TRANSLATE || activeMode === MODE.CHANGE)
-        moveImage(e);
-    };
+		const handleMouseUp = (e) => {
+			e.preventDefault();
+			selectMode();
+		};
 
-    $canvas.mousedown(handleMouseDown);
-    $canvas.mousemove(handleMouseMove);
-    $canvas.mouseup(handleMouseUp);
+		const handleMouseMove = (e) => {
+			if (editingIndex < 0 || !activeMode) return;
+			e.preventDefault();
+			if (activeMode === MODE.TRANSLATE || activeMode === MODE.CHANGE)
+				moveImage(e);
+		};
 
-    const touchEvent = (e) => {
-      const r = $canvas[0].getBoundingClientRect();
-      const touch =
-        e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
-      e.offsetX = touch.clientX - r.left;
-      e.offsetY = touch.clientY - r.top;
+		$canvas.mousedown(handleMouseDown);
+		$canvas.mousemove(handleMouseMove);
+		$canvas.mouseup(handleMouseUp);
 
-      return e;
-    };
+		const touchEvent = (e) => {
+			const r = $canvas[0].getBoundingClientRect();
+			const touch =
+				e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
+			e.offsetX = touch.clientX - r.left;
+			e.offsetY = touch.clientY - r.top;
 
-    $canvas.on("touchstart", (e) => handleMouseDown(touchEvent(e)));
-    $canvas.on("touchmove", (e) => handleMouseMove(touchEvent(e)));
-    $canvas.on("touchend", (e) => handleMouseUp(touchEvent(e)));
+			return e;
+		};
 
-    const changeScale = (val) => {
-      const editingArea = areas[editingIndex];
-      if (editingArea) {
-        editingArea.image.scale = Number(val);
-        drawArea();
-      }
-      return $canvas;
-    };
+		$canvas.on("touchstart", (e) => handleMouseDown(touchEvent(e)));
+		$canvas.on("touchmove", (e) => handleMouseMove(touchEvent(e)));
+		$canvas.on("touchend", (e) => handleMouseUp(touchEvent(e)));
 
-    const changeRotate = (val) => {
-      const editingArea = areas[editingIndex];
-      if (editingArea) {
-        editingArea.image.rotate = Number(val);
-        drawArea();
-      }
-      return $canvas;
-    };
+		const changeScale = (val) => {
+			const editingArea = areas[editingIndex];
+			if (editingArea) {
+				editingArea.image.scale = Number(val);
+				drawArea();
+			}
+			return $canvas;
+		};
 
-    const createPhoto = (type) => $canvas[0].toDataURL(type || "image/png");
+		const changeRotate = (val) => {
+			const editingArea = areas[editingIndex];
+			if (editingArea) {
+				editingArea.image.rotate = Number(val);
+				drawArea();
+			}
+			return $canvas;
+		};
 
-    const setDefault = () => {
-      editingIndex = -1;
-      activeMode = null;
-      $canvas.css("cursor", "auto");
-      startX = 0;
-      startY = 0;
-      lastClickDate = null;
-      drawArea();
-    };
+		const createPhoto = (type) => $canvas[0].toDataURL(type || "image/png");
 
-    const init = async () => {
-      const areasMapping = await Promise.all(options.images.map(initArea));
-      areas.push(...areasMapping);
-      drawArea();
-    };
+		const setDefault = () => {
+			editingIndex = -1;
+			activeMode = null;
+			$canvas.css("cursor", "auto");
+			startX = 0;
+			startY = 0;
+			lastClickDate = null;
+			drawArea();
+		};
 
-    init();
+		const init = async () => {
+			const areasMapping = await Promise.all(options.images.map(initArea));
+			areas.push(...areasMapping);
+			drawArea();
+		};
 
-    return {
-      changeScale,
-      changeRotate,
-      createPhoto,
-      setDefault
-    };
-  };
+		init();
+
+		return {
+			changeScale,
+			changeRotate,
+			createPhoto,
+			setDefault
+		};
+	};
 })(jQuery);
