04cf5d48d90bedf9d380c6eb81c9b3cfe2d0e3dd
enes.sefa.k <enes.sefa.k@gmail.com>
2020-06-30 01:05:21 +0300

    change demo

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ $(document).ready(function () {
   const $canvasTable = $("#canvas").canvasTable({
     width: 400,
     height: 400,
-    gap: 5,
+    gap: 10,
     strokeDash: 5,
     strokeWidth: 2,
     showAlignmentLines: true,
-    frameWidth: 5,
     // css grid template areas
-    templateAreas: ["photo1 photo1 photo1", "photo2 photo3 photo3"],
+    templateAreas: ["photo1 photo1 photo2 photo2", "photo3 photo3 photo4 photo5", "photo6 photo6 photo6 photo6"],
     images: [
       {
         area: "photo1",
@@ -24,6 +23,21 @@ $(document).ready(function () {
         area: "photo3",
         source:
           "https://images.unsplash.com/photo-1588691551142-3da6178a482e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+      },
+      {
+        area: "photo4",
+        source:
+          "https://images.unsplash.com/photo-1588231793676-7bf33c466184?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+      },
+      {
+        area: "photo5",
+        source:
+          "https://images.unsplash.com/photo-1586854146097-6d9fb7b031fc?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+      },
+      {
+        area: "photo6",
+        source:
+          "https://images.unsplash.com/photo-1587467238707-0f9b326d56bd?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
       }
     ],
     onSelected(selectedArea) {
@@ -57,6 +71,4 @@ $(document).ready(function () {
       previewWindow.document.write(canvasImage.outerHTML);
     };
   });
-
-  // $canvasTable.changeFrame('https://i.ibb.co/fDzCXr7/cerceve.jpg', 25)
 });
